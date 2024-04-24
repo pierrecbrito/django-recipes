0592c62b1f39655a5642b0c6db01bfe3bcd642d1
Pierre Brito <33200451+pierrecbrito@users.noreply.github.com>
2024-04-23 21:34:54 -0300

    Testes de view e URL

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,6 +1,38 @@
 from django.test import TestCase
-
+from django.urls import reverse, resolve
+from recipes import views
 
 class RecipeURLsTest(TestCase):
-    def test_the_pytest_is_ok(self):
-        assert 1 == 1, 'Um é igual a um'
+    """
+        Testes relativos a renderização da URL correta.
+    """
+    def test_recipe_home_url_is_correct(self):
+        home_url = reverse('recipes:home')
+        self.assertEqual(home_url, '/')
+    
+    def test_recipe_category_url_is_correct(self):
+        category_url = reverse('recipes:category', kwargs={'category_id': 1})
+        self.assertEqual(category_url, '/recipes/category/1/')
+
+    def test_recipe_detail_url_is_correct(self):
+        detail_url = reverse('recipes:recipe', kwargs={'id': 1})
+        self.assertEqual(detail_url, '/recipes/1/')
+
+class RecipeViewsTest(TestCase):
+    """
+        Testes de função view correta
+    """
+    def test_recipe_home_views_function_is_correct(self):
+        view = resolve(reverse('recipes:home'))
+        self.assertIs(view.func, views.home)
+    
+    def test_recipe_category_views_function_is_correct(self):
+        view = resolve(reverse('recipes:category', kwargs={'category_id': 1}))
+        self.assertIs(view.func, views.category)
+    
+    def test_recipe_detail_views_function_is_correct(self):
+        view = resolve(reverse('recipes:recipe', kwargs={'id': 1}))
+        self.assertIs(view.func, views.recipe)
+
+
+    
